a22fd1ce03eaa8bd55da24f132411d21d41a7401
AdarshVMore <skullcrushermore@gmail.com>
2023-04-21 20:52:26 +0530

    eachDicApplication render logic done in adminHome

--- src/components/adminHome/AdminHome.jsx
+++ src/components/adminHome/AdminHome.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import "./admin.css";
+import EachDocApplication from "../eachDocApplication/EachDocApplication";
 
 function AdminHome() {
   const [selectedBtn, setSelectedBtn] = useState(0);
   const [heading, setHeading] = useState("");
+  const [urlIndex, setUrlIndex] = useState("");
   const [data, setData] = useState([
     {
-      name: "Alice",
+      name: "Jhony",
       time: "2023-04-19 10:30",
       state: "new",
     },
@@ -23,7 +27,7 @@ function AdminHome() {
     {
       name: "David",
       time: "2023-04-21 16:00 ",
-      state: "rejected",
+      state: "new",
     },
     {
       name: "Eve",
@@ -70,145 +74,229 @@ function AdminHome() {
 
   return (
     <div className="admin">
-      <div className="btns">
-        <button
-          id={`${selectedBtn === 0 ? "selected" : ""}`}
-          onClick={() => {
-            setSelectedBtn(0);
-          }}
-        >
-          All
-        </button>
-        <button
-          id={`${selectedBtn === 1 ? "selected" : ""}`}
-          onClick={() => {
-            setSelectedBtn(1);
-          }}
-        >
-          New Applications
-        </button>
-        <button
-          id={`${selectedBtn === 2 ? "selected" : ""}`}
-          onClick={() => {
-            setSelectedBtn(2);
-          }}
-        >
-          UnderVerification
-        </button>
-        <button
-          id={`${selectedBtn === 3 ? "selected" : ""}`}
-          onClick={() => {
-            setSelectedBtn(3);
-          }}
-        >
-          Verified
-        </button>
-        <button
-          id={`${selectedBtn === 4 ? "selected" : ""}`}
-          onClick={() => {
-            setSelectedBtn(4);
-          }}
-        >
-          Rejected
-        </button>
-      </div>
-      <div className="listOfApplicants">
-        <div className="top">
-          <p className="heading">{heading}</p>
-          <div className="colors">
-            <div className="c new"></div>
-            <div className="c under-verification"></div>
-            <div className="c verified"></div>
-            <div className="c rejected"></div>
-          </div>
-        </div>
-
-        <div className="allList">
-          {selectedBtn === 0 &&
-            data.map((item, index) => (
-              <div className="eachList" key={index}>
-                <div className="docName">
-                  Dr. {selectedBtn === 0 ? item.name : ""}
+      <Routes>
+        <Route
+          path={`/Doc${urlIndex}`}
+          element={<EachDocApplication data={data[urlIndex]} />}
+        />
+        <Route
+          path=""
+          element={
+            <>
+              <div className="btns">
+                <button
+                  id={`${selectedBtn === 0 ? "selected" : ""}`}
+                  onClick={() => {
+                    setSelectedBtn(0);
+                  }}
+                >
+                  All
+                </button>
+                <button
+                  id={`${selectedBtn === 1 ? "selected" : ""}`}
+                  onClick={() => {
+                    setSelectedBtn(1);
+                  }}
+                >
+                  New Applications
+                </button>
+                <button
+                  id={`${selectedBtn === 2 ? "selected" : ""}`}
+                  onClick={() => {
+                    setSelectedBtn(2);
+                  }}
+                >
+                  UnderVerification
+                </button>
+                <button
+                  id={`${selectedBtn === 3 ? "selected" : ""}`}
+                  onClick={() => {
+                    setSelectedBtn(3);
+                  }}
+                >
+                  Verified
+                </button>
+                <button
+                  id={`${selectedBtn === 4 ? "selected" : ""}`}
+                  onClick={() => {
+                    setSelectedBtn(4);
+                  }}
+                >
+                  Rejected
+                </button>
+              </div>
+              <div className="listOfApplicants">
+                <div className="top">
+                  <p className="heading">{heading}</p>
+                  <div className="colors">
+                    <div
+                      className="c new"
+                      onClick={() => {
+                        setSelectedBtn(0);
+                      }}
+                    ></div>
+                    <div
+                      className="c under-verification"
+                      onClick={() => {
+                        setSelectedBtn(2);
+                      }}
+                    ></div>
+                    <div
+                      className="c verified"
+                      onClick={() => {
+                        setSelectedBtn(3);
+                      }}
+                    ></div>
+                    <div
+                      className="c rejected"
+                      onClick={() => {
+                        setSelectedBtn(4);
+                      }}
+                    ></div>
+                  </div>
                 </div>
-                <div className="right">
-                  <div className="timeDate">{item.time.toString()}</div>
-                  {item.state === "new" ? <div className="c new"></div> : ""}
-                  {item.state === "under verification" ? (
-                    <div className="c under-verification"></div>
-                  ) : (
-                    ""
+
+                <div className="allList">
+                  {selectedBtn === 0 &&
+                    data.map((item, index) => (
+                      <Link
+                        to={`/admin/Doc${index}`}
+                        onClick={() => setUrlIndex(index)}
+                      >
+                        <div className="eachList" key={index}>
+                          <div className="docName">
+                            Dr. {selectedBtn === 0 ? item.name : ""}
+                          </div>
+                          <div className="right">
+                            <div className="timeDate">
+                              {item.time.toString()}
+                            </div>
+                            {item.state === "new" ? (
+                              <div className="c new"></div>
+                            ) : (
+                              ""
+                            )}
+                            {item.state === "under verification" ? (
+                              <div className="c under-verification"></div>
+                            ) : (
+                              ""
+                            )}
+                            {item.state === "verified" ? (
+                              <div className="c verified"></div>
+                            ) : (
+                              ""
+                            )}
+                            {item.state === "rejected" ? (
+                              <div className="c rejected"></div>
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        </div>
+                      </Link>
+                    ))}
+                  {selectedBtn === 1 && (
+                    <div>
+                      {data
+                        // .filter((item) => item.state === "new")
+                        .map((item, index) => {
+                          if (item.state === "new") {
+                            return (
+                              <Link
+                                to={`/admin/Doc${index}`}
+                                onClick={() => setUrlIndex(index)}
+                              >
+                                <div className="eachList" key={index}>
+                                  <div className="docName">Dr. {item.name}</div>
+                                  <div className="right">
+                                    <div className="timeDate">
+                                      {item.time.toString()}
+                                    </div>
+                                  </div>
+                                </div>
+                              </Link>
+                            );
+                          }
+                        })}
+                    </div>
                   )}
-                  {item.state === "verified" ? (
-                    <div className="c verified"></div>
-                  ) : (
-                    ""
+                  {selectedBtn === 2 && (
+                    <div>
+                      {data
+                        // .filter((item) => item.state === "under verification")
+                        .map((item, index) => {
+                          if (item.state === "under verification") {
+                            return (
+                              <Link
+                                to={`/admin/Doc${index}`}
+                                onClick={() => setUrlIndex(index)}
+                              >
+                                <div className="eachList" key={index}>
+                                  <div className="docName">Dr. {item.name}</div>
+                                  <div className="right">
+                                    <div className="timeDate">
+                                      {item.time.toString()}
+                                    </div>
+                                  </div>
+                                </div>
+                              </Link>
+                            );
+                          }
+                        })}
+                    </div>
                   )}
-                  {item.state === "rejected" ? (
-                    <div className="c rejected"></div>
-                  ) : (
-                    ""
+                  {selectedBtn === 3 && (
+                    <div>
+                      {data.map((item, index) => {
+                        if (item.state === "verified") {
+                          return (
+                            <Link
+                              to={`/admin/Doc${index}`}
+                              onClick={() => setUrlIndex(index)}
+                            >
+                              <div className="eachList" key={index}>
+                                <div className="docName">Dr. {item.name}</div>
+                                <div className="right">
+                                  <div className="timeDate">
+                                    {item.time.toString()}
+                                  </div>
+                                </div>
+                              </div>
+                            </Link>
+                          );
+                        }
+                      })}
+                    </div>
+                  )}
+                  {selectedBtn === 4 && (
+                    <div>
+                      {data.map((item, index) => {
+                        if (item.state === "rejected") {
+                          return (
+                            <Link
+                              to={`/admin/Doc${index}`}
+                              onClick={() => setUrlIndex(index)}
+                            >
+                              <div className="eachList" key={index}>
+                                <div className="docName">Dr. {item.name}</div>
+                                <div className="right">
+                                  <div className="timeDate">
+                                    {item.time.toString()}
+                                  </div>
+                                </div>
+                              </div>
+                            </Link>
+                          );
+                        }
+                      })}
+                    </div>
                   )}
                 </div>
               </div>
-            ))}
-          {selectedBtn === 1 && (
-            <div>
-              {data
-                .filter((item) => item.state === "new")
-                .map((item, index) => (
-                  <div className="eachList" key={index}>
-                    <div className="docName">Dr. {item.name}</div>
-                    <div className="right">
-                      <div className="timeDate">{item.time.toString()}</div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )}
-          {selectedBtn === 2 && (
-            <div>
-              {data
-                .filter((item) => item.state === "under verification")
-                .map((item, index) => (
-                  <div className="eachList" key={index}>
-                    <div className="docName">Dr. {item.name}</div>
-                    <div className="right">
-                      <div className="timeDate">{item.time.toString()}</div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )}
-          {selectedBtn === 3 && (
-            <div>
-              {data
-                .filter((item) => item.state === "verified")
-                .map((item, index) => (
-                  <div className="eachList" key={index}>
-                    <div className="docName">Dr. {item.name}</div>
-                    <div className="right">
-                      <div className="timeDate">{item.time.toString()}</div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )}
-          {selectedBtn === 4 && (
-            <div>
-              {data
-                .filter((item) => item.state === "rejected")
-                .map((item, index) => (
-                  <div className="eachList" key={index}>
-                    <div className="docName">Dr. {item.name}</div>
-                    <div className="right">
-                      <div className="timeDate">{item.time.toString()}</div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-      </div>
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/eachDocApplication/EachDocApplication.jsx
+++ src/components/eachDocApplication/EachDocApplication.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function EachDocApplication() {
-  return <div>EachDocApplication</div>;
+function EachDocApplication({ data }) {
+  return (
+    <div>
+      <div className="sidebar-backButton">
+        <button>
+          <Link to="/admin">Back</Link>
+        </button>
+      </div>
+      <div className="eachDocApplication"></div>
+    </div>
+  );
 }
 
 export default EachDocApplication;
